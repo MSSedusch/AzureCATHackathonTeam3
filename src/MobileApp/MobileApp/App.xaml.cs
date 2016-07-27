e93d598ad80d201895942a650befcc158fddb848
Sebastian Max Dusch <sedusch@microsoft.com>
2016-07-27 11:54:44 -0700

    add api and sf projects

--- src/MobileApp/MobileApp/App.xaml.cs
+++ src/MobileApp/MobileApp/App.xaml.cs
@@ -11,7 +11,7 @@ namespace MobileApp
     {
         public App()
         {
-            InitializeComponent();
+            //InitializeComponent();
 
             MainPage = new MobileApp.MainPage();
         }
--- src/MobileApp/MobileApp/MainPage.xaml.cs
+++ src/MobileApp/MobileApp/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,16 @@ namespace MobileApp
 {
     public partial class MainPage : ContentPage
     {
+        public Dictionary<AssetType, List<Asset>> Assets { get; set; }
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void LoadAssets()
+        {
+            //WebClient
         }
     }
 }
